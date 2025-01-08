63a52be347593c0e48df42b48644973bf124f19c
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2025-01-08 21:02:46 +0300

    fix: linter and refactoring

--- lib/ecampus_web/live/dashboard/index.ex
+++ lib/ecampus_web/live/dashboard/index.ex
@@ -11,14 +11,26 @@ defmodule EcampusWeb.Dashboard.Index do
   @impl true
   def mount(_params, %{"locale" => locale} = _session, socket) do
     Gettext.put_locale(EcampusWeb.Gettext, locale)
+    incoming = Classes.get_incoming_class(socket.assigns.current_user)
+    stats = Classes.get_stats(socket.assigns.current_user)
+
+    stats = %{
+      stats
+      | last_quizzes:
+          stats.last_quizzes
+          |> Enum.with_index()
+          |> Enum.map(fn {lq, idx} -> {"last_quizzes-#{idx}", lq} end)
+    }
+
+    ranking =
+      Classes.rank_students_in_group(socket.assigns.current_user.group_id)
+      |> Enum.with_index()
+      |> Enum.map(fn {r, idx} -> {"ranking-#{idx}", r} end)
 
     {:ok,
      socket
-     |> assign(:incoming, Classes.get_incoming_class(socket.assigns.current_user))
-     |> assign(
-       :stats,
-       Classes.get_stats(socket.assigns.current_user)
-     )
-     |> assign(:ranking, Classes.rank_students_in_group(socket.assigns.current_user.group_id))}
+     |> assign(:incoming, incoming)
+     |> assign(:stats, stats)
+     |> assign(:ranking, ranking)}
   end
 end
